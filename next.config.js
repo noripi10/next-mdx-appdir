0d9a0fc7524182bc40c2118023c8c02ec45c7d4f
sugiyama <sgym.snk@gmail.com>
2023-08-24 18:53:30 +0900

    add comment

--- next.config.js
+++ next.config.js
@@ -6,10 +6,13 @@ const nextConfig = {
   },
 };
 
+// ★rehype-pretty-code
 // https://claritydev.net/blog/copy-to-clipboard-button-nextjs-mdx-rehype
 // https://rehype-pretty-code.netlify.app/
 const rehypePrettyCode = require('rehype-pretty-code');
 
+// Theme(light, dark)２つ分のコンポーネントが作られる
+// Toggle Button での　Root Class (.light .dark)で表示を切り替える
 /** @type {import('rehype-pretty-code').Options} */
 const options = {
   theme: {
